57c692707563858cc437190f2ea55efa50d00432
Maximilian Algehed <m.algehed@gmail.com>
2018-11-13 15:32:39 +0100

    Added principal instance to DC-labels

--- src/Lattice/DCLabels.hs
+++ src/Lattice/DCLabels.hs
@@ -3,4 +3,14 @@ module Lattice.DCLabels where
 import Lattice.Class
 import Lattice.CNF
 
-type DCLabels a = (CNF a, Dual (CNF a))
+newtype DCLabels a = DC (CNF a, Dual (CNF a))
+
+instance Principal DCLabels where
+  singleton a = DC (singleton a , Dual (singleton a))
+
+instance Ord a => Lattice (DCLabels a) where
+  DC l0 `lub` DC l1 = DC $ l0 `lub` l1
+  DC l0 `glb` DC l1 = DC $ l0 `glb` l1
+  top = DC top
+  bot = DC bot
+  DC l0 `canFlowTo` DC l1 = l0 `canFlowTo` l1
